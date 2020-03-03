24fa73107efbb057a6e852302087071d008c3454
ARIANY <ariany_mendes@yahoo.com>
2020-03-03 14:04:04 -0500

    add the contacts and print it

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,2 +1,12 @@
 class Api::ContactsController < ApplicationController
+  def show_contact
+    @contact = Contact.first
+    render 'show.json.jb'
+  end
+
+  def every_contact
+    @contacts = Contact.all
+    render 'index.json.jb'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/the_contacts" => "contacts#show_contact"
+    get "/all_contacts" => "contacts#every_contact"
+
+  end
 end
